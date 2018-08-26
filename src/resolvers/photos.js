460628d162ec6f04363280844bc4eda3f4504158
airburst <mark@fairhursts.net>
2018-08-26 21:40:22 +0100

    fix: tidy up resolver

--- src/resolvers/photos.js
+++ src/resolvers/photos.js
@@ -5,25 +5,19 @@ const { Op } = Sequelize;
 
 export default {
   Query: {
-    allPhotos: (parent, args, { models, userId = 1 }) => {
+    allPhotos: (parent, args, { models, userId = 1 }) =>
     // Authorisation
     // if (!ctx.session) {
     //   return Error('You are not authorised to do this.');
     // }
-      console.log(userId);
-
-      return models.Photo.findAll({
-        // attributes: ['url'],
+      models.Photo.findAll({
         where: { userId: { [Op.eq]: userId } },
       })
         .then(result => ({
           data: result.map(r => r.dataValues),
           errors: null,
         }))
-        .catch(err => ({ data: [], errors: formatErrors(err, models) }));
-    },
+        .catch(err => ({ data: [], errors: formatErrors(err, models) }))
+    ,
   },
 };
-
-// if (!result) { return []; }
-// return result.map(r => r.dataValues).map(r => Object.values(r)[0]);
