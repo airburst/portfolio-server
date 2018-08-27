5d39fdd8289632e14dff6efea4f9aced7a2ebf8d
airburst <mark@fairhursts.net>
2018-08-27 17:34:58 +0100

    temp: Add delay to each upload

--- src/services/processFile.js
+++ src/services/processFile.js
@@ -1,10 +1,12 @@
 import getExifData from './exif';
 import { resizeImage } from './resize';
+import { delay } from './batch';
 
 export default file => new Promise(async (resolve, reject) => {
   try {
     const exif = await getExifData(file);
     const { thumbnail, urls, error } = await resizeImage(file, exif);
+    await delay(5000); // TODO: remove
     resolve({
       exif, error, urls, thumbnail,
     });
