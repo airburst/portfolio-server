7d04c7858d9c40013313167b6bb28b99009e9f74
Mark Fairhurst <mfairhurst@cmtgroup.com>
2020-06-19 22:26:19 +0100

    fix: Sequelize model

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -1,68 +1,71 @@
 import Sequelize from 'sequelize';
 import sequelize from './sequelize';
 
-const Photo = sequelize.define('photos', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  urls: {
-    type: Sequelize.JSON,
-    allowNull: false,
-  },
-  thumbnail: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  title: Sequelize.STRING,
-  caption: Sequelize.STRING,
-  width: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  height: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  exposure: Sequelize.INTEGER,
-  shutter: Sequelize.INTEGER,
-  aperture: Sequelize.INTEGER,
-  iso: Sequelize.INTEGER,
-  focalLength: Sequelize.INTEGER,
-  dateTaken: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    defaultValue: '1990-01-01 00:00:00+00',
-  },
-  isPublic: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-    defaultValue: true,
-  },
-  bin: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-},
-{
-  indexes: [
-    {
-      name: 'i_photo_name',
-      unique: true,
-      fields: ['name'],
+const Photo = sequelize.define(
+  'photos',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      name: 'i_photo_title',
-      unique: true,
-      fields: ['title'],
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-  ],
-});
+    urls: {
+      type: Sequelize.JSON,
+      allowNull: false,
+    },
+    thumbnail: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    title: Sequelize.STRING,
+    caption: Sequelize.STRING,
+    width: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    height: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    exposure: Sequelize.FLOAT,
+    shutter: Sequelize.FLOAT,
+    aperture: Sequelize.FLOAT,
+    iso: Sequelize.INTEGER,
+    focalLength: Sequelize.INTEGER,
+    dateTaken: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: '1990-01-01 00:00:00+00',
+    },
+    isPublic: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    bin: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    indexes: [
+      {
+        name: 'i_photo_name',
+        unique: true,
+        fields: ['name'],
+      },
+      {
+        name: 'i_photo_title',
+        unique: true,
+        fields: ['title'],
+      },
+    ],
+  },
+);
 
 export default Photo;
